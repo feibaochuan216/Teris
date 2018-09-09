fece4713a6801377c9332c01594bfc1ef0ccb6b7
yuyanze1992 <260722287@qq.com>
2018-09-09 12:26:13 +0800

    Signed-off-by: yuyanze1992 <260722287@qq.com>

--- Teris1_8A/logindialog.cpp
+++ Teris1_8A/logindialog.cpp
@@ -3,6 +3,7 @@
 #include <QPushButton>
 #include <QDialogButtonBox>
 #include "./show/GameDlg.h"
+#include <QPalette>
 
 LoginDialog::LoginDialog(QWidget *parent) :
     QDialog(parent),
@@ -13,6 +14,10 @@ LoginDialog::LoginDialog(QWidget *parent) :
             this,SLOT(on_buttonBox_accepted()));
     connect(ui->buttonBox,SIGNAL(rejected()),
             this,SLOT(on_buttonBox_rejected()));*/
+    QPalette pe;
+    pe.setColor(QPalette::WindowText, Qt::yellow);
+    ui->label_2->setPalette(pe);
+    ui->label_3->setPalette(pe);
 }
 
 LoginDialog::~LoginDialog()
@@ -22,12 +27,20 @@ LoginDialog::~LoginDialog()
 
 void LoginDialog::on_buttonBox_accepted()
 {
-    qDebug("登录成功!");
-    close();
-    GameDlg w;
-    w.exec();
+    if(ui->UserEdit->text() == "csd1710" && ui->PasswdEdit->text() == "5211314"){
+        qDebug("登录成功!");
+        close();
+        GameDlg w;
+        w.exec();
+    }
+    else{
+        QMessageBox msgbox(QMessageBox::Critical,windowTitle(),"用户名或密码有误！",QMessageBox::Ok,this);
+        msgbox.setButtonText(QMessageBox::Ok,"确定");
+        ui->UserEdit->setText("");
+        ui->PasswdEdit->setText("");
+        msgbox.exec();
 
-
+    }
 }
 void LoginDialog::on_buttonBox_rejected()
 {
