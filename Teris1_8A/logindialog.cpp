d4a80c44888f51ce81ffd2f91d7836afc594a4dc
yuyanze1992 <260722287@qq.com>
2018-09-09 10:58:16 +0800

    添加登录界面

---
+++ Teris1_8A/logindialog.cpp
@@ -0,0 +1,45 @@
+#include "logindialog.h"
+#include "ui_logindialog.h"
+#include <QPushButton>
+#include <QDialogButtonBox>
+#include "./show/GameDlg.h"
+
+LoginDialog::LoginDialog(QWidget *parent) :
+    QDialog(parent),
+    ui(new Ui::LoginDialog)
+{
+    ui->setupUi(this);
+    /*connect(ui->buttonBox,SIGNAL(accepted()),
+            this,SLOT(on_buttonBox_accepted()));
+    connect(ui->buttonBox,SIGNAL(rejected()),
+            this,SLOT(on_buttonBox_rejected()));*/
+}
+
+LoginDialog::~LoginDialog()
+{
+    delete ui;
+}
+
+void LoginDialog::on_buttonBox_accepted()
+{
+    qDebug("登录成功!");
+    close();
+    GameDlg w;
+    w.exec();
+
+
+}
+void LoginDialog::on_buttonBox_rejected()
+{
+    close();
+}
+
+void LoginDialog::paintEvent(QPaintEvent *)
+{
+    QPainter painter(this);
+    QRect rcImage = ui->frame->frameRect();
+    rcImage.translate(ui->frame->pos());
+    QImage image(":/timg.jpg");
+    painter.drawImage(rcImage,image);
+
+}
---
+++ Teris1_8A/logindialog.h
@@ -0,0 +1,31 @@
+#ifndef LOGINDIALOG_H
+#define LOGINDIALOG_H
+
+#include <QDialog>
+#include <QPainter>
+#include <QMessageBox>
+
+namespace Ui {
+class LoginDialog;
+}
+
+class LoginDialog : public QDialog
+{
+    Q_OBJECT
+
+public:
+    explicit LoginDialog(QWidget *parent = 0);
+    ~LoginDialog();
+
+private slots:
+    void on_buttonBox_accepted();
+
+    void on_buttonBox_rejected();
+
+private:
+    void paintEvent(QPaintEvent *);
+private:
+    Ui::LoginDialog *ui;
+};
+
+#endif // LOGINDIALOG_H
